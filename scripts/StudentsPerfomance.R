25381589430ad7ac8ba9d93e7d8ec8aa18827e04
jxareas <areasjx@gmail.com>
2022-07-25 20:53:47 -0600

    Students Performance T test

--- scripts/StudentsPerfomance.R
+++ scripts/StudentsPerfomance.R
@@ -3,6 +3,8 @@
 
 library(data.table)
 library(janitor)
+library(tidyr)
+library(ggtheme)
 library(tidyverse)
 
 df <- fread("./data/StudentsPerformance.csv")
@@ -12,3 +14,31 @@ df <- fread("./data/StudentsPerformance.csv")
 df <- clean_names(df) %>% 
     mutate(average=(math_score+reading_score+writing_score)/3) %>% 
     select(c(gender, average))
+
+
+# Plot --------------------------------------------------------------------
+
+ggplot(data = df, mapping = aes(x = average, color = gender)) +
+  geom_density() +
+  labs(
+    title = "Average Scores",
+    x = "Score",
+    y = "Density"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(hjust = .5, face = "bold"),
+  )
+
+
+# T Test setup -------------------------------------------------------------------
+
+M <-
+  df %>% 
+  filter(gender == "male") %>% select(average)
+
+F <-
+  df %>% 
+  filter(gender == "female") %>% select(average)
+
+t.test(M, F)
