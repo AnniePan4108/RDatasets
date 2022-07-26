7a2982da5ed8c77594e2c71e3640b2eaf81db8f3
Annie Pan <105067975+AnniePan4108@users.noreply.github.com>
2022-07-25 22:31:26 -0500

    ANOVA

---
+++ scripts/Insurance.R
@@ -0,0 +1,21 @@
+
+# Loading Packages/Setup --------------------------------------------------
+
+library(tidyverse)
+library(janitor)
+library(tidyr)
+library(ggplot2)
+library(data.table)
+df <- fread("./data/insurance.csv") %>% 
+  clean_names() %>% 
+  select(region, charges)
+
+# Plot --------------------------------------------------------------------
+
+ggplot(data=df, mapping=aes(x=region, y=charges, color=region))+
+  geom_boxplot()
+
+# ANOVA -------------------------------------------------------------------
+
+fit <- lm(data=df, formula=charges~region)
+anova(fit)
